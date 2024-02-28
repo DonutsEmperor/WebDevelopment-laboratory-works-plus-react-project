332a1d5c0c32834b97eb30561ba78172480787b0
NikitaDonkey <polovykhnikita@gmail.com>
2024-02-28 10:33:54 +0700

    firstTask+

---
+++ async_js/index.js
@@ -0,0 +1,72 @@
+
+///////////////////////// first task
+//counter(3);
+function counter(n){
+    for(let b = n; b >= 0; b--){
+        setTimeout(() => console.log(b), b * 1000);
+    }
+}
+
+//counter2(3);
+function counter2(n){
+    let newN = n;
+    const inrval = setInterval(() => {
+        console.log(newN);
+        newN--;
+        if(newN < 0) clearInterval(inrval);
+    }, 1000);
+}
+
+
+///////////////////////// first.second task
+class Timer {
+    constructor(n) {
+        this.start_value = n;
+        this.timer = undefined;
+        this.value = n; 
+    }
+
+    start(){
+        this.timer = setInterval(() => {
+            console.log(this.value);
+            this.value--;
+            if (this.value < 0) {
+                this.stop();
+            }
+        }, 1000);
+    }
+
+    pause(){
+        clearInterval(this.timer);
+        this.timer = undefined;
+    }
+
+    stop(){
+        this.pause()
+        this.value = this.start_value;
+    }
+}
+
+function createCounter(){
+    return new Timer(50);
+}
+
+let timer = createCounter();
+timer.start();
+
+setTimeout(() => {
+    console.log('pause')
+    timer.pause()
+
+}, 10000);
+
+setTimeout(() => {
+    console.log('start')
+    timer.start();
+
+}, 20000);
+
+setTimeout(() => {
+    console.log('stop')
+    timer.stop()
+}, 30000);
