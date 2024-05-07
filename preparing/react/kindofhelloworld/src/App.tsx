73f7d904f0edfc2963854fe2e164424d6fe0ef99
NikitaDonkey <polovykhnikita@gmail.com>
2024-05-08 00:21:39 +0700

    taking up to react

---
+++ preparing/react/kindofhelloworld/src/App.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const Square: React.FC<{number: number}> = ({ number}) => {
+  return ( <p>The square of {number} is: {number * number}</p> );
+};
+
+const OnlyEven: React.FC<{array: Array<number>}> = ({ array }) => {
+  let newArray = [];
+  for (let index = 0; index < array.length; index++) {
+    if(array[index] % 2 === 0){
+      newArray.push(array[index], ",");
+    }
+  }
+  if(newArray[newArray.length - 1] === ",") newArray.pop();
+
+  return (
+  <p>The array of OnlyEven sequence is {newArray}</p>
+)};
+
+const Temperature: React.FC<{ degrees: number}> = ({ degrees }) => {
+  const mycolor = degrees >= 0 ? 'red' : 'blue';
+  return (
+  <p style={{color: mycolor}}>Today temperature = {degrees} C</p>
+)};
+
+const CoolButton = () => {
+  const [color, setColor] = useState('green');
+
+  const handleButtonClick = () => {
+    const newColor = color === 'green' ? 'red' : 'green';
+    setColor(newColor);
+  };
+
+  return (
+    <button style={{ background: color }} onClick={handleButtonClick}>
+      Click me to change my text color
+    </button>
+  );
+};
+
+const Default = () => {
+  return (
+    <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+  );
+};
+
+
+function App() {
+  return (
+    <div className="App">
+      <Square number={3} />
+      <OnlyEven array={Array(1, 2, 3, 4)} />
+      <Temperature degrees={36.6} />
+      <CoolButton />
+    </div>
+  );
+}
+
+export default App;
